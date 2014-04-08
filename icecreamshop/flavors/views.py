51f3a39165a761c812b39eecf069703c16bdf7ce
nimiq <puntonim@gmail.com>
2014-04-08 22:47:19 +0200

    Imports order template

--- icecreamshop/flavors/views.py
+++ icecreamshop/flavors/views.py
@@ -1,4 +1,14 @@
+# Stdlib imports
+# E.g.: from math import sqrt
+
+# Core Django imports
 from django.shortcuts import render
+
+# Third-party app imports
+# E.g.: from django_extensions.db.models import TimeStampedModel
+
+# Imports from local apps
+# E.g.: from .models import BananaSplit
 
 
 def home(request, template='home.html'):
